49fd65b436f76b0a87333f1549dd088d11e5551c
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-03-07 12:36:39 -0400

    [TSK] Layout Windgets Transferencias [NEW] Criando o layout e sua class para a Transferencias

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Transferencias',
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
+          appBar: AppBar(title: Text('Transferencias')),
+          floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
+          body: ListaTransferencia()),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 4)),
+        ItemTransferencia(Transferencia(200.0, 3)),
+        ItemTransferencia(Transferencia(300.0, 2)),
+        ItemTransferencia(Transferencia(400.0, 1))
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
