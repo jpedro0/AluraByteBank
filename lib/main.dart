d7512aea19efe90cc45cdf4ac1a1f4a5ca12a1dd
Joao Pedro Brito <jpedro0brito@gmail.com>
2020-03-07 11:11:26 -0400

    inicializando commits

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Transferencias',
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
